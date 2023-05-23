3de4033a2738e1fabcabb0a3926cbcb84c42368f
Tayamul <rai_tayamul@hotmail.com>
2023-05-23 22:06:12 +0100

    Delete unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Client } from "appwrite";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const client = new Client();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject("646d0604df6385bc7d16");
+  
+root.render(<App />);
