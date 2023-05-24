b953fc4bd716066c0a0ffe255d5589d8c1fd1e72
Tayamul <rai_tayamul@hotmail.com>
2023-05-24 19:18:29 +0100

    Add default styling to the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import './nav.css'
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav>
       <ul className='nav-list'>
-        <li>Home</li>
-        <li>Tracker</li>
+        <Link to='/home'><li>Home</li></Link>
+        <Link to='/tracker'><li>Tracker</li></Link>
       </ul>
     </nav>
   );
