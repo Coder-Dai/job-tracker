df659360b6b31fb826fb676fbf28d57b962ff5c6
Tayamul <rai_tayamul@hotmail.com>
2023-05-24 19:17:42 +0100

    Create a basic layout for the home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import './home.css'
+
+const Home = () => {
+    return (
+        <main>
+            <h1> Welcome to Job Tracker App.</h1>
+            <h3> Click on Tracker menu to get started!</h3>
+        </main>
+    )
+}
+
+export default Home;
