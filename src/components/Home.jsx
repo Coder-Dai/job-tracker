e96a968594cb14bfb96075d9de3e9f28ad5957b4
Tayamul <rai_tayamul@hotmail.com>
2023-06-01 17:41:15 +0100

    Editing home section completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,16 +5,16 @@ const Home = () => {
     <main id="home">
       <div id="home-container">
         <div id="home-header">
-          <h1>HOME</h1>
+          <h1>Home</h1>
         </div>
         <div id="home-content">
-          <h2>Welcome back, User!</h2>
+          <h2>Welcome back User!</h2>
           <div id="home-body">
             <div id="home-body-left">
               <h3>Upcoming:</h3>
               <p><b>02/03/23 - </b>Software Engineer- Apple</p>
               <p><b>03/03/23 - </b> Front End Developer- Google</p>
-              <p><b>04/03/23 -</b> Go Developer- Netflix</p>
+              <p><b>04/03/23 - </b> Go Developer- Netflix</p>
             </div>
             <div id="home-body-right">CHART</div>
           </div>
