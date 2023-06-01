4c8668ab20c547e0ac0c38665ab45f3122b01813
gurungpratik <pratikgurung365@gmail.com>
2023-06-01 16:27:28 +0100

    Table component styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,45 +1,48 @@
 import React, { useState } from "react";
+import "./table.css";
 import data from "../mock-table-data.json";
 
 const Table = () => {
   const [jobs, setJobs] = useState(data);
 
   return (
-    <section className="table-container">
-      <table>
-        <thead>
-          <tr>
-            <th>
-              <input type="checkbox"></input>
-            </th>
-            <th>Position</th>
-            <th>Company</th>
-            <th>Salary</th>
-            <th>Location</th>
-            <th>Follow-up</th>
-            <th>Deadline</th>
-            <th>Status</th>
-            <th>Excitement</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jobs.map((job) => (
+    <section>
+      <div className="table-container">
+        <table>
+          <thead>
             <tr>
-              <td>
+              <th>
                 <input type="checkbox"></input>
-              </td>
-              <td>{job.position}</td>
-              <td>{job.company}</td>
-              <td>{job.salary}</td>
-              <td>{job.location}</td>
-              <td>{job.followUp}</td>
-              <td>{job.deadline}</td>
-              <td>{job.status}</td>
-              <td>{job.excitement}</td>
+              </th>
+              <th>Position</th>
+              <th>Company</th>
+              <th>Salary</th>
+              <th>Location</th>
+              <th>Follow-up</th>
+              <th>Deadline</th>
+              <th>Status</th>
+              <th>Excitement</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {jobs.map((job) => (
+              <tr>
+                <td>
+                  <input type="checkbox"></input>
+                </td>
+                <td>{job.position}</td>
+                <td>{job.company}</td>
+                <td>{job.salary}</td>
+                <td>{job.location}</td>
+                <td>{job.followUp}</td>
+                <td>{job.deadline}</td>
+                <td>{job.status}</td>
+                <td>{job.excitement}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </section>
   );
 };
