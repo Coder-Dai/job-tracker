712d5ee298edd57a7caa1a2a89658921fa6794bf
Tayamul <rai_tayamul@hotmail.com>
2023-06-05 21:00:25 +0100

    Add value and onChange attribute in each input fields

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,15 +5,15 @@ import "./table.css";
 const Table = ({ jobsList, setJobsList }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [position, setPosition] = useState('')
-  const [company, setCompany] = useState('')
-  const [salary, setSalary] = useState('')
-  const [location, setLocation] = useState('')
-  const [followup, setFollowup] = useState('')
-  const [deadline, setDeadline] = useState('')
-  const [status, setStatus] = useState('')
-  const [excitement, setExcitement] = useState('')
-  const [description, setDescription] = useState('')
+  const [position, setPosition] = useState("");
+  const [company, setCompany] = useState("");
+  const [salary, setSalary] = useState("");
+  const [location, setLocation] = useState("");
+  const [followup, setFollowup] = useState("");
+  const [deadline, setDeadline] = useState("");
+  const [status, setStatus] = useState("");
+  const [excitement, setExcitement] = useState("");
+  const [description, setDescription] = useState("");
 
   async function postJob() {
     await addJobAsync("testing-123");
@@ -33,26 +33,115 @@ const Table = ({ jobsList, setJobsList }) => {
           </button>
           {isModalOpen && (
             <div id="form-container">
-              <button onClick={() => {setIsModalOpen(false)}}>X</button>
+              <button
+                onClick={() => {
+                  setIsModalOpen(false);
+                }}
+              >
+                X
+              </button>
               <h2>Add a New Job Post</h2>
-              <form id='form-modal'>
+              <form id="form-modal" onSubmit={addJob}>
                 <label htmlFor="position">Position</label>
-                <input id="position" type="text" placeholder="Position"></input>
+                <input
+                  id="position"
+                  type="text"
+                  value={position}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setPosition(e.target.value);
+                  }}
+                  placeholder="Position"
+                ></input>
                 <label htmlFor="company">Company</label>
-                <input id="Company" type="text" placeholder="Company"></input>
+                <input
+                  id="Company"
+                  type="text"
+                  value={company}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setCompany(e.target.value);
+                  }}
+                  placeholder="Company"
+                ></input>
                 <label htmlFor="salary">Salary</label>
-                <input id="salary" type="number" placeholder="Salary"></input>
+                <input
+                  id="salary"
+                  type="number"
+                  value={salary}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setSalary(e.target.value);
+                  }}
+                  placeholder="Salary"
+                ></input>
                 <label htmlFor="location">Location</label>
-                <input id="location" type="text" placeholder="Location"></input>
+                <input
+                  id="location"
+                  type="text"
+                  value={location}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setLocation(e.target.value);
+                  }}
+                  placeholder="Location"
+                ></input>
                 <label htmlFor="followup">Follow up</label>
-                <input id="followup" type="date" placeholder="Follow up"></input>
+                <input
+                  id="followup"
+                  type="date"
+                  value={followup}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setFollowup(e.target.value);
+                  }}
+                  placeholder="Follow up"
+                ></input>
                 <label htmlFor="deadline">Deadline</label>
-                <input id="deadline" type="date" placeholder="Deadline"></input>
+                <input
+                  id="deadline"
+                  type="date"
+                  value={deadline}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setDeadline(e.target.value);
+                  }}
+                  placeholder="Deadline"
+                ></input>
                 <label htmlFor="status">Status</label>
-                <input id="status" type="text" placeholder="Status"></input>
+                <input
+                  id="status"
+                  type="text"
+                  value={status}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setStatus(e.target.value);
+                  }}
+                  placeholder="Status"
+                ></input>
                 <label htmlFor="excitement">Excitement</label>
-                <input id="excitement" type="text" placeholder="Excitement"></input>
-                <button type='submit'>Sumbit</button>
+                <input
+                  id="excitement"
+                  type="text"
+                  value={excitement}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setExcitement(e.target.value);
+                  }}
+                  placeholder="Excitement"
+                ></input>
+                <label htmlFor="description">Job Description</label>
+                <input
+                  id="description"
+                  type="textfield"
+                  value={description}
+                  onChange={(e) => {
+                    console.log(e.target.value);
+                    setDescription(e.target.value);
+                  }}
+                  placeholder="Excitement"
+                ></input>
+                <button type="submit">Sumbit</button>
               </form>
             </div>
           )}
