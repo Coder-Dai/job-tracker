ae5c24ab5ba8e4ccef8892dcdeeda185e29f3758
Tayamul <rai_tayamul@hotmail.com>
2023-06-07 19:01:27 +0100

    Add functionality of closing the form when it is submitted

--- src/components/JobForm.jsx
+++ src/components/JobForm.jsx
@@ -2,92 +2,99 @@ import { useContext } from "react";
 import { addJobAsync } from "../api/dataService";
 import UserContext from "../contexts/userContext";
 
-const JobForm = ({setIsModalOpen, setAllJobs, jobsList, setJobsList}) => {
-    const { userId } = useContext(UserContext);
+const JobForm = ({ setIsModalOpen, setAllJobs, jobsList, setJobsList }) => {
+  const { userId } = useContext(UserContext);
 
-    async function postJob(e) {
-        e.preventDefault();
-    
-        if (userId) {
-          const docId = crypto.randomUUID();
-    
-          const job = {
-            $id: docId,
-            userId,
-            position: e.target[0].value,
-            company: e.target[1].value,
-            salary: e.target[2].value,
-            location: e.target[3].value,
-            followUp: new Date(e.target[4].value).toISOString(),
-            deadline: new Date(e.target[5].value).toISOString(),
-            status: "INTERESTED",
-            excitement: 1,
-          };
-    
-          const newJobsList = [...jobsList];
-          newJobsList.push(job);
-    
-          setAllJobs(newJobsList);
-          setJobsList(newJobsList);
-    
-          await addJobAsync(job);
-        } else {
-          console.log("userId is null");
-        }
-      }
+  async function postJob(e) {
+    e.preventDefault();
+    setIsModalOpen(false);
 
-    return (
-        <div id="form-container">
-              <button
-                onClick={() => {
-                  setIsModalOpen(false);
-                }}
-                id="form-close-btn"
-              >
-                X
-              </button>
-              <h2 id="form-heading">Add a New Job Post</h2>
-              <form id="form-modal" onSubmit={postJob}>
-                <div>
-                  <label htmlFor="position">Position</label>
-                  <input id="position" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="company">Company</label>
-                  <input id="Company" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="salary">Salary</label>
-                  <input id="salary" type="number"></input>
-                </div>
-                <div>
-                  <label htmlFor="location">Location</label>
-                  <input id="location" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="followup">Follow up</label>
-                  <input id="followup" type="date"></input>
-                </div>
-                <div>
-                  <label htmlFor="deadline">Deadline</label>
-                  <input id="deadline" type="date"></input>
-                </div>
-                <div>
-                  <label htmlFor="status">Status</label>
-                  <input id="status" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="excitement">Excitement</label>
-                  <input id="excitement" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="description">Job Description</label>
-                  <input id="description" type="textfield"></input>
-                </div>
-                <button type="submit" id='form-submit-btn'>Save</button>
-              </form>
-            </div>
-    )
-}
+
+    if (userId) {
+      const docId = crypto.randomUUID();
+
+      const job = {
+        $id: docId,
+        userId,
+        position: e.target[0].value,
+        company: e.target[1].value,
+        salary: e.target[2].value,
+        location: e.target[3].value,
+        followUp: new Date(e.target[4].value).toISOString(),
+        deadline: new Date(e.target[5].value).toISOString(),
+        status: "INTERESTED",
+        excitement: 1,
+      };
+
+      const newJobsList = [...jobsList];
+      newJobsList.push(job);
+
+      setAllJobs(newJobsList);
+      setJobsList(newJobsList);
+
+      await addJobAsync(job);
+    } else {
+      console.log("userId is null");
+    }
+  }
+
+  return (
+    <div id="form-container">
+      <button
+        onClick={() => {
+          setIsModalOpen(false);
+        }}
+        id="form-close-btn"
+      >
+        X
+      </button>
+      <h2 id="form-heading">Add a New Job Post</h2>
+      <form id="form-modal" onSubmit={postJob}>
+        <div>
+          <label htmlFor="position">Position</label>
+          <input id="position" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="company">Company</label>
+          <input id="Company" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="salary">Salary</label>
+          <input id="salary" type="number"></input>
+        </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <input id="location" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="followup">Follow up</label>
+          <input id="followup" type="date"></input>
+        </div>
+        <div>
+          <label htmlFor="deadline">Deadline</label>
+          <input id="deadline" type="date"></input>
+        </div>
+        <div>
+          <label htmlFor="status">Status</label>
+          <input id="status" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="excitement">Excitement</label>
+          <input id="excitement" type="text"></input>
+        </div>
+        <div>
+          <label htmlFor="description">Job Description</label>
+          <input id="description" type="textfield"></input>
+        </div>
+        <button
+          type="submit"
+          id="form-submit-btn"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default JobForm;
