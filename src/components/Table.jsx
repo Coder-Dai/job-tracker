431740598ea69e4036a3763446f3078edbc05761
Tayamul <rai_tayamul@hotmail.com>
2023-06-07 18:46:49 +0100

    Import JobForm component into the table and send props down

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from "react";
 import { addJobAsync, deleteJobAsync } from "../api/dataService";
 import UserContext from "../contexts/userContext";
+import JobForm from "./JobForm";
 import "./table.css";
 
 const Table = ({ setAllJobs, jobsList, setJobsList }) => {
@@ -64,58 +65,7 @@ const Table = ({ setAllJobs, jobsList, setJobsList }) => {
           <button className="add-button" onClick={openForm}>
             + Add job
           </button>
-          {isModalOpen && (
-            <div id="form-container">
-              <button
-                onClick={() => {
-                  setIsModalOpen(false);
-                }}
-                id="form-close-btn"
-              >
-                X
-              </button>
-              <h2 id="form-heading">Add a New Job Post</h2>
-              <form id="form-modal" onSubmit={postJob}>
-                <div>
-                  <label htmlFor="position">Position</label>
-                  <input id="position" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="company">Company</label>
-                  <input id="Company" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="salary">Salary</label>
-                  <input id="salary" type="number"></input>
-                </div>
-                <div>
-                  <label htmlFor="location">Location</label>
-                  <input id="location" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="followup">Follow up</label>
-                  <input id="followup" type="date"></input>
-                </div>
-                <div>
-                  <label htmlFor="deadline">Deadline</label>
-                  <input id="deadline" type="date"></input>
-                </div>
-                <div>
-                  <label htmlFor="status">Status</label>
-                  <input id="status" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="excitement">Excitement</label>
-                  <input id="excitement" type="text"></input>
-                </div>
-                <div>
-                  <label htmlFor="description">Job Description</label>
-                  <input id="description" type="textfield"></input>
-                </div>
-                <button type="submit" id='form-submit-btn'>Save</button>
-              </form>
-            </div>
-          )}
+          {isModalOpen && <JobForm setIsModalOpen={setIsModalOpen}/>}
         </div>
         <table>
           <thead>
